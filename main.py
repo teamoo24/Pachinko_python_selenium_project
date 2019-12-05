03431613085277b9bbb9d642d4bc3b7ecf13fc8b
shibainu <601107w@gmail.com>
2019-12-05 16:50:29 +0900

    コメント追加

--- main.py
+++ main.py
@@ -10,22 +10,29 @@ driver_path = "driver/chromedriver"
 # 接続先のurl指定
 url = 'http://dedama.me/kc_chuo/'
 
+# クロームのオプションを保存するオブジェクト追加
 options = webdriver.ChromeOptions()
 
 # 画面を浮かばずに作動
 #options.add_argument('--headless')
 #driver = webdriver.Chrome(driver_path, options=options)
 
+# 保存しておいたChrome driver呼び出し(ver.78対応)
 driver = webdriver.Chrome(driver_path)
 
+# ブラウザ立ち上げ
 driver.get(url)
 
+# 5秒待ち
 time.sleep(5)
+
+# ファイルセーブ用のファイル名指定
 nowtime = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
 
+# 全体的なページのソースをdownloadページに保存
 with open('download/test_'+nowtime+'.html','w',encoding='utf-8') as f:
 	f.write(driver.page_source)
 
 
-time.sleep(5)
+# 終了
 driver.quit()
