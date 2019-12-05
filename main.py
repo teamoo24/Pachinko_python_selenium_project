f1cb5f3a795647ca70d71cf1e00b35cd0ef3b230
shibainu <601107w@gmail.com>
2019-12-05 16:23:30 +0900

    サイトのhtmlのスクラップ機能実装

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import urllib.request
 import time
 from selenium import webdriver
+from datetime import datetime
 
 # ドライブ位置設定
 driver_path = "driver/chromedriver"
@@ -9,12 +10,22 @@ driver_path = "driver/chromedriver"
 # 接続先のurl指定
 url = 'http://dedama.me/kc_chuo/'
 
+options = webdriver.ChromeOptions()
+
+# 画面を浮かばずに作動
+#options.add_argument('--headless')
+#driver = webdriver.Chrome(driver_path, options=options)
+
 driver = webdriver.Chrome(driver_path)
 
 driver.get(url)
+
 time.sleep(5)
-search_box = driver.find_element_by_name("q")
-search_box.send_keys('ChromeDriver')
-search_box.submit()
+nowtime = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+
+with open('download/test_'+nowtime+'.html','w') as f:
+	f.write(driver.page_source)
+
+
 time.sleep(5)
 driver.quit()
